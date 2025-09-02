150375df2443d5338522cd8926575d9c7e12bf14
LIU Tie <tie.liu@foxmail.com>
2025-09-02 20:49:58 +0800

    fixed small bug.

--- duplicate_detection/utiles.py
+++ duplicate_detection/utiles.py
@@ -1,5 +1,7 @@
 def get_human_readable_size(size_bytes):
     """将字节大小转换为更易读的格式"""
+    if size_bytes is None:
+        return "0(Input is None)"
     for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
         if size_bytes < 1024.0:
             return f"{size_bytes:.2f} {unit}"
